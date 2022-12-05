fb552f32ccc50439dbc021406178b8fb1c124565
Elijah Spicer <eli.sushi.spicer@gmail.com>
2022-12-04 20:27:12 -0500

    Initial Commit

---
+++ Text Mode Editor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Text Mode Editor
+//
+//  Created by Elijah Spicer on 12/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text Mode Editor/Text_Mode_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Mode_EditorApp.swift
+//  Text Mode Editor
+//
+//  Created by Elijah Spicer on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Text_Mode_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
