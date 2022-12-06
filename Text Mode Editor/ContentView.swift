73a74f4062f7e60af66993e0ecd2650123386403
Elijah Spicer <eli.sushi.spicer@gmail.com>
2022-12-05 22:37:26 -0500

    color is now based on the two selections

--- Text Mode Editor/ContentView.swift
+++ Text Mode Editor/ContentView.swift
@@ -8,29 +8,29 @@
 import SwiftUI
 
 
-func getPixelData(tile: UInt8) -> [UInt8] {
+func getPixelData(tile: UInt8, front: Color, back: Color) -> [UInt8] {
     var pixelData = [UInt8](repeating: 0, count: 192)
     for i in stride(from:0, to: 192, by: 3) {
         if i % 2 == 0 {
-            pixelData[i] = 46
-            pixelData[i+1] = 67
-            pixelData[i+2] = 255
+            pixelData[i] = UInt8(UIColor(front).cgColor.components![0]*255)
+            pixelData[i+1] = UInt8(UIColor(front).cgColor.components![1]*255)
+            pixelData[i+2] = UInt8(UIColor(front).cgColor.components![2]*255)
         } else {
-            pixelData[i] = 255
-            pixelData[i+1] = 67
-            pixelData[i+2] = 46
+            pixelData[i] = UInt8(UIColor(back).cgColor.components![0]*255)
+            pixelData[i+1] = UInt8(UIColor(back).cgColor.components![1]*255)
+            pixelData[i+2] = UInt8(UIColor(back).cgColor.components![2]*255)
         }
     }
     return pixelData
 }
 
 
-func MakeImage(tile: UInt8) -> CGImage {
+func MakeImage(tile: UInt8, front: Color, back: Color) -> CGImage {
     let height = 8
     let width = 8
     let numComponents = 3
     let numBytes = height * width * numComponents
-    let pixelData = getPixelData(tile: tile)
+    let pixelData = getPixelData(tile: tile, front: front, back: back)
     let colorspace = CGColorSpaceCreateDeviceRGB()
     let rgbData = CFDataCreate(nil, pixelData, numBytes)!
     let provider = CGDataProvider(data: rgbData)!
@@ -51,12 +51,16 @@ func MakeImage(tile: UInt8) -> CGImage {
 
 struct ContentView: View {
     @State var showingTileSelect = false
-    @State private var primary = Color.white
-    @State private var accent = Color.black
+    @State private var front = Color.white
+    @State private var back = Color.black
 
     
     var body: some View {
-        Image(MakeImage(tile: 32), scale: (1/3), label: Text(""))
+        Image(MakeImage(tile: 32,
+                        front: front,
+                        back: back),
+              scale: (1/3),
+              label: Text(""))
             .interpolation(Image.Interpolation.none)
         HStack {
             Button(action: {
@@ -66,8 +70,8 @@ struct ContentView: View {
             }.sheet(isPresented: $showingTileSelect) {
                 TileSelectView()
             }.buttonStyle(.borderedProminent)
-            ColorPicker("Primary", selection: $primary)
-            ColorPicker("Accent", selection: $accent)
+            ColorPicker("Primary", selection: $front)
+            ColorPicker("Accent", selection: $back)
         }.frame(maxWidth: .infinity,
                 maxHeight: .infinity,
                 alignment: .topLeading)
